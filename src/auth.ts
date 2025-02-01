6df15fe1f14f33e5cab37730cdf634067a56f761
Avijit <abhijitpradhan909@gmail.com>
2025-02-01 21:28:30 +0530

    make runtime of auth as nodejs

--- src/auth.ts
+++ src/auth.ts
@@ -1,13 +1,28 @@
+export const config = {
+  runtime: "nodejs",
+};
 import NextAuth from "next-auth";
 import Google from "next-auth/providers/google";
 import Github from "next-auth/providers/github";
-
-import { registerNewUser } from "./lib/actions/resgisterNewUser";
+import { User } from "./models/users.model";
+import { dbConnect } from "./lib/db/db-connect";
+import { run } from "node:test";
 export const { handlers, signIn, signOut, auth } = NextAuth({
   providers: [Google, Github],
   callbacks: {
     async signIn({ user }) {
-      await registerNewUser(user);
+      await dbConnect();
+
+      const existingUser = await User.findOne({ email: user.email });
+      console.log("existingUser", existingUser);
+      if (!existingUser) {
+        await User.create({
+          email: user.email,
+          name: user.name,
+          avatarImage: user.image,
+        });
+      }
+
       return true;
     },
   },
